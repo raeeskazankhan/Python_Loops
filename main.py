8bc0bb64b435dfdd389334d83b1025d53469b13a
raeeskazankhan <m215027016@gmail.com>
2021-12-18 17:10:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+
+# AVERAGE HEIGHT
+#
+# student_height = input("Input a list of student heights: ").split()
+# for n in range(0, len(student_height)):
+#     student_height[n] = int(student_height[n])
+# print(student_height)
+#
+# total_sum = 0
+# for height in student_height:
+#     total_sum += height
+#
+# average_height = round(total_sum / int(len(student_height)))
+# print(f"AVERAGE HEIGHT: {average_height}m")
+
+# HIGHEST CLASS SCORE
+#
+# student_scores = input("INPUT A LIST OF STUDENT SCORES: ").split()
+# for n in range(0, len(student_scores)):
+#     student_scores[n] = int(student_scores[n])
+# print(student_scores)
+#
+# highest_score = student_scores[0]
+# for score in range(0, len(student_scores)):
+#     if highest_score <= student_scores[score]:
+#         highest_score = student_scores[score]
+#
+# print(f"THE HIGHEST SCORE IN THE CLASS IS: {highest_score}")
+
+# ADDING EVEN NUMBERS 1-100
+#
+# adding_even_numbers = 0
+# for n in range(0, 101, 2):
+#     print(n)
+#     adding_even_numbers += n
+#
+# print(f"TOTAL SUM OF EVEN NUMBERS: {adding_even_numbers}")
+
+# FIZZBUZZ APP
+# for n in range(1, 101):
+#     if n % 3 == 0 and n % 5 == 0:
+#         print("FizzBuzz")
+#     elif n % 3 == 0:
+#         print("Fizz")
+#     elif n % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(n)
+
+
